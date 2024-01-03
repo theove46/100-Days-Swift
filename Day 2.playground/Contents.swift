e4e31f604d21ff6171387bda08c128597b527455
Md. Ohiduzzaman <57717227+theove46@users.noreply.github.com>
2024-01-03 12:25:33 +0600

    Complete Day 2

---
+++ Day 2.playground/Contents.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+beatles[1]
+
+let colors = Set(["red", "green", "blue"])
+let colors2 = Set(["red", "green", "blue", "red", "blue"])
+
+var name = (first: "Taylor", last: "Swift")
+name.0
+name.1
+name.first
+name.last
+
+let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
+let set = Set(["aardvark", "astronaut", "azalea"])
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+
+let heights = [
+    "Taylor Swift": 1.78,
+    "Ed Sheeran": 1.73
+]
+heights["Taylor Swift"]
+
+let favoriteIceCream = [
+    "Paul": "Chocolate",
+    "Sophie": "Vanilla"
+]
+favoriteIceCream["Paul"]
+favoriteIceCream["Charlotte"]
+favoriteIceCream["Charlotte", default: "Mango"]
+
+var teams = [String: String]()
+teams["Paul"] = "Red"
+
+var results = [Int]()
+var words = Set<String>()
+var numbers = Set<Int>()
+
+var scores = Dictionary<String, Int>()
+var results2 = Array<Int>()
+
+let result = "failure"
+let result2 = "failed"
+let result3 = "fail"
+enum Result {
+    case success
+    case failure
+}
+let result4 = Result.failure
+
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+let talking = Activity.talking(topic: "football")
+
+enum Planet: Int {
+    case mercury
+    case venus
+    case earth
+    case mars
+}
+let earth = Planet(rawValue: 2)
+
+enum Planet2: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
