77939dae6aa999675f4d92bfc018341008600883
Md. Ohiduzzaman <57717227+theove46@users.noreply.github.com>
2024-01-03 12:25:05 +0600

    Complete Day 1

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "Hello, ove"
+
+var num = 25
+var population = 8_00_0000_0
+
+var str1 = """
+hello
+world
+"""
+var str2 = """
+hello \
+world
+"""
+
+var pi = 3.141
+var good = true
+var score = 68
+
+var marks = "The Marks: \(score)"
+var result = "The Result: \(marks)"
+let taylor = "Taylor swift"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
