97ddbfa4ad2a52034f2e79b35dff64c4b08c261d
Md. Ohiduzzaman <57717227+theove46@users.noreply.github.com>
2024-01-13 22:46:41 +0600

    Compele Project 7

---
+++ Project 7 Tab Bar Controller/Project 7/Project 7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project 7
+//
+//  Created by BS1098 on 10/1/24.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project 7 Tab Bar Controller/Project 7/Project 7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project 7
+//
+//  Created by BS1098 on 10/1/24.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
