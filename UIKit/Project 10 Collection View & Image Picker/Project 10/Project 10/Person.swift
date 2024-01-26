336d6338f96d7295d43fba9fb13bf521fa40b4c3
Md. Ohiduzzaman <57717227+theove46@users.noreply.github.com>
2024-01-26 15:49:24 +0600

    Complete Project 10

---
+++ UIKit/Project 10 Collection View & Image Picker/Project 10/Project 10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by BS1098 on 26/1/24.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
