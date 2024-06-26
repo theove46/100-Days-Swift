c4bb8aceda6fd62f1d5768faaf4174c3c8bf4a8f
Md. Ohiduzzaman <57717227+theove46@users.noreply.github.com>
2024-06-26 13:18:48 +0600

    Complete Project 12

---
+++ UIKit/Project 12 Userdefalts NSCoding and Codable/Project 12b/Project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by BS1098 on 25/1/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
